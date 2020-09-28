f9efb6ebdf5aeef6ca442f98794a4e83a96498a2
Sparrow517 <ig.tarantin2013@yandex.ru>
2020-09-28 21:20:52 +0300

    Add files via upload

---
+++ lection01.py
@@ -0,0 +1,7 @@
+a = float(input('Введи длину первой стороны треугольника \n'))
+b = float(input('Введи длину второй стороны треугольника \n'))
+c = float(input('Введи угол между этими сторонами в градуасх \n'))
+import math
+c = math.radians(c)
+cos = math.cos(c)
+print ("Третья сторона равна " + str(a*b*cos))
